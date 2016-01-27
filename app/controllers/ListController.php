cc59bf4f0480089e8d61d045dbe1f9a712d62c8e
Xavier Philbert <x.philbert@pap.fr>
2016-01-27 15:35:07 +0100

    Study Sabre DAV Server in ListController

--- app/controllers/ListController.php
+++ app/controllers/ListController.php
@@ -14,15 +14,15 @@ class ListController extends Controller
 		$server = new DAV\Server($rootDirectory);
 
 		// If your server is not on your webroot, make sure the following line has the correct information
-		$server->setBaseUri('/list');
+		$server->setBaseUri('/list/index');
 
 		// The lock manager is responsible for making sure users don't overwrite each others changes.
-		$lockBackend = new DAV\Locks\Backend\File('../data/locks');
-		$lockPlugin  = new DAV\Locks\Plugin($lockBackend);
-		$server->addPlugin($lockPlugin);
+		//$lockBackend = new DAV\Locks\Backend\File('../data/locks');
+		//$lockPlugin  = new DAV\Locks\Plugin($lockBackend);
+		//$server->addPlugin($lockPlugin);
 
 		// This ensures that we get a pretty index in the browser, but it is optional.
-		$server->addPlugin(new DAV\Browser\Plugin(false));
+		//$server->addPlugin(new DAV\Browser\Plugin(false));
 
 		// All we need to do now, is to fire up the server
 		$server->exec();
